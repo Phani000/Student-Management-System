bd17ed4db6607372ccbbaddc466d4302b02c4593
Phani000 <phanishekarreddy116@gmail.com>
2025-06-09 17:01:49 +0530

    first commit

---
+++ src/main/java/net/javaguides/sms/StudentManagementSystemApplication.java
@@ -0,0 +1,43 @@
+package net.javaguides.sms;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import net.javaguides.sms.entity.Student;
+import net.javaguides.sms.repository.StudentRepository;
+
+@SpringBootApplication
+public class StudentManagementSystemApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemApplication.class, args);
+	}
+
+	@Autowired
+	private StudentRepository studentRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		/*
+	 	Student student1 = new Student("luffy", "monkey", "luffy@gmail.com");
+		studentRepository.save(student1);
+		
+		Student student2 = new Student("ace", "portagus", "ace@gmail.com");
+		studentRepository.save(student2);
+		
+		Student student3 = new Student("sabo", "D", "sabo@gmail.com");
+		studentRepository.save(student3);
+		*/
+	}
+
+	public StudentRepository getStudentRepository() {
+		return studentRepository;
+	}
+
+	public void setStudentRepository(StudentRepository studentRepository) {
+		this.studentRepository = studentRepository;
+	}
+
+}
